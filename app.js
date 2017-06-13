91e92f6d059d4db11d370e9247b3edee988930c1
Evan Harris <echarris@smcm.edu>
2017-06-13 12:02:40 -0400

    Adds average function

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ function min(numbers) {
    }
       return minimum;
 }
+
+function average(numbers) {
+  var sum = 0;
+  var i = 0;
+  while (i < numbers.length) {
+    sum += numbers.length;
+  }
+  return sum/numbers.length;
+}
+
+function average(numbers) {
+  var sum = 0;
+  var i = 0;
+  while (i < numbers.length) {
+    sum += numbers[i];
+    // console.log(i);
+    i++;
+  }
+  return sum/numbers.length;
+}

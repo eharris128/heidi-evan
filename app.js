ad31ec63d822fe85b5bc1a030db174b087cc5a54
Evan Harris <echarris@smcm.edu>
2017-06-13 12:04:00 -0400

    Removes additional average function

--- app.js
+++ app.js
@@ -26,17 +26,7 @@ function average(numbers) {
   var sum = 0;
   var i = 0;
   while (i < numbers.length) {
-    sum += numbers.length;
-  }
-  return sum/numbers.length;
-}
-
-function average(numbers) {
-  var sum = 0;
-  var i = 0;
-  while (i < numbers.length) {
     sum += numbers[i];
-    // console.log(i);
     i++;
   }
   return sum/numbers.length;
